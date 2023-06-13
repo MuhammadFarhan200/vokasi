15c7f0168c9ec1cfe042c2c11ebeb410f494cc12
MuhammadFarhan200 <farhan_200@smkassalaambandung.sch.id>
2023-06-13 07:09:09 +0700

    optimizing

--- resources/views/themes/web/head.blade.php
+++ resources/views/themes/web/head.blade.php
@@ -7,4 +7,14 @@
     <link rel="stylesheet" href="{{ asset('owl-carousel/dist/assets/owl.theme.default.min.css') }}">
     <link rel="stylesheet" href="{{ asset('web/css/myCss.css') }}">
     <title>{{config('app.name') . ': ' .$title ?? config('app.name')}}</title>
+    <style>
+        .cms_web {
+            opacity: 1;
+            transition-duration: 0.3s;
+        }
+
+        html.is-animating .cms_web {
+            opacity: 0;
+        }
+    </style>
 </head>
